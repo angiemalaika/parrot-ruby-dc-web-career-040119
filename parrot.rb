d0f3d0ab6525b72377fa9f036b395b48241fab93
Angela Mumba <malaika.mumba@gmail.com>
2019-03-26 04:40:01 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(parrot="squawk!")
-  puts "I like birds"
-   
+  puts "I like birds #{parrot}"
+  parrot
   
 end
   
